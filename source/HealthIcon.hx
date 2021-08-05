9aa4fc8db0e96b1ab185bb79195822db665fcf84
aired-98 <aidanjredden@gmail.com>
2021-08-05 01:37:32 +0100

    Dialogue box icons are pretty cool

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -50,7 +50,7 @@ class HealthIcon extends FlxSprite
 	public function changeIcon(char:String)
 	{
 		if (char != 'bf-pixel' && char != 'bf-old')
-			char = char.split("-")[0];
+			char = char.split('-')[0];
 
 		loadGraphic(Paths.image('icons/icon-' + char), true, 150, 150);
 		animation.add(char, [0, 1], 0, false, isPlayer);
